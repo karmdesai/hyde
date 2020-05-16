dfa8673cddc0bfe8dee4011882b033da0e7bf9ed
Karm Desai <karmdesai18@gmail.com>
2020-05-16 11:55:04 -0400

    testing page actions

--- background.js
+++ background.js
@@ -1,43 +1,13 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    var tabUrl = changeInfo.url;
+function checkForValidUrl(tabId, changeInfo, tab) {
+    var tabUrl = tab.url;
+    var onYT = tabUrl.includes("youtube.com");
 
-    try {
-        var onYT = tabUrl.includes("youtube.com/watch?v");
-    }
-
-    catch(err) {
-        console.log(err);
-    }
-
+    // if the user is on YouTube
     if (onYT === true) {
-        alert(tabUrl);
-
-        // start logic
-        var s = document.getElementById('stylehidecontrols'); 
-
-        if (s) {
-            s.remove();
-        } 
-        
-        else { 
-            s = document.createElement('style'); 
-            s.id = 'stylehidecontrols'; 
-            var r = '#movie_player .ytp-gradient-top, #movie_player .ytp-chrome-top, #movie_player .ytp-progress-bar-container, #movie_player .ytp-chrome-controls{display:none !important}, #movie_player .ytp-gradient-bottom'; 
-            s.appendChild(document.createTextNode(r)); 
-            document.body.appendChild(s);
-        }
-
-        removeElementsByClass('ytp-gradient-bottom');
-        
-        void 0;
-        // end logic
+        // show the page action
+        chrome.pageAction.show(tabId);
     }
-})
+};
 
-function removeElementsByClass(className) {
-    var elements = document.getElementsByClassName(className);
-
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
-    }
-}
+// listen for any changes to the URL of any tab
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
