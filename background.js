051b5f183662449b5fe7192b110e3a940176480a
Karm Desai <karmdesai18@gmail.com>
2020-05-16 12:06:33 -0400

    tested pageAction using declarativeContent

--- background.js
+++ background.js
@@ -1,13 +1,19 @@
-function checkForValidUrl(tabId, changeInfo, tab) {
-    var tabUrl = tab.url;
-    var onYT = tabUrl.includes("youtube.com");
-
-    // if the user is on YouTube
-    if (onYT === true) {
-        // show the page action
-        chrome.pageAction.show(tabId);
-    }
-};
-
-// listen for any changes to the URL of any tab
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
+// when the extension is installed or upgraded...
+chrome.runtime.onInstalled.addListener(function() {
+    // replace all rules...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      // with a new rule...
+      chrome.declarativeContent.onPageChanged.addRules([
+        {
+          // that fires when a page's URL contains 'youtube.com/watch?v'...
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { urlContains: 'youtube.com/watch?v' },
+            })
+          ],
+          // and shows the extension's page action.
+          actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        }
+      ]);
+    });
+});
